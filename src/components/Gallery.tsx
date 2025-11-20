928d00fe9ab96b2ec1b35d769035db9dfb03a23b
fitwaysamsun <fitwaysamsun@gmail.com>
2025-11-20 20:58:49 +0300

    Update Gallery.tsx

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -47,8 +47,8 @@ const Gallery = () => {
           }
         }
 
-        setYenimahalleImages(allImages.slice(0, 26));
-        setMimarsinanImages(allImages.slice(26));
+        setYenimahalleImages(allImages.slice(0, 28));
+        setMimarsinanImages(allImages.slice(28));
       } catch (error) {
         console.error("Error fetching gallery data:", error);
       }
